fa9cadc93611fd695209833fd0316fff0b0806d0
Paul Nechifor <0000signal@gmail.com>
2018-10-09 12:38:24 +0300

    - added the blank bussiness project - added the blank bussiness tests project - updated all nuget packages

---
+++ PaymentProcessor/PaymentProcessor.Bussiness.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PaymentProcessor.Bussiness.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PaymentProcessor.Bussiness.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d6ab094b-d83f-4eb2-a5cf-05cb3dad3db3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
